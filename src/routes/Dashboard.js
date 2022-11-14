e4d207ed1fca28d2ef9c765a5a598b019e17a6af
ULCE <ulcechrist.pro@gmail.com>
2022-11-14 23:18:22 +0100

    Login / logout logic with an idle state in case to consider network delays

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { NavBar } from '../components/Nav/NavBar';
 
-export const Dashboard = () => {
-  return <div>Dashboard</div>;
+import Session from 'supertokens-web-js/recipe/session';
+import { UserRoleClaim } from 'supertokens-web-js/recipe/userroles';
+import { Container } from '@chakra-ui/react';
+
+async function getSessionPayload() {}
+
+async function doesSessionExist() {
+  if (await Session.doesSessionExist()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const Dashboard = () => {
+  const [sessionExist, setSessionExist] = useState(undefined);
+  /**
+   * create a context that will have the session info
+   */
+  useEffect(() => {
+    console.log('useEffect');
+    const session = async () => {
+      setSessionExist(await doesSessionExist());
+    };
+    session();
+  }, []);
+  return sessionExist === true ? (
+    <Fragment>
+      <NavBar />
+      <Container>
+        <Outlet />
+      </Container>
+    </Fragment>
+  ) : sessionExist === false ? (
+    <Navigate to="/" />
+  ) : (
+    <div>Loading</div>
+  );
 };
+
+export default Dashboard;
